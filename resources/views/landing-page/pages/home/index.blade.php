14c762bc1c66fd4f9d9eff55c46527343c7fead5
Dhevajaya <dhevajaya123@gmail.com>
2024-08-23 09:34:08 +0700

    video

--- resources/views/landing-page/pages/home/index.blade.php
+++ resources/views/landing-page/pages/home/index.blade.php
@@ -1,169 +1,185 @@
-@extends("landing-page.layouts.main")
+@extends('landing-page.layouts.main')
 
-@section("title","Home")
+@section('title', 'Home')
 
-@section("css")
+@section('css')
 @endsection
 
-@section("preloader")
-    @component("landing-page.components.preloader")
+@section('preloader')
+    @component('landing-page.components.preloader')
     @endcomponent
 @endsection
 
-@section("content")
-<!--====== Start Hero Section ======-->
-<section class="hero-area hero-style-three bg_cover" style="background-image: url({{URL::to('/')}}/templates/landing-page/assets/images/hero/bg.png);">
-    <div class="shape shape-one scene"><span data-depth="3"></span></div>
-    <div class="shape shape-two scene"><span data-depth="5"></span></div>
-    <div class="shape shape-three scene"><span data-depth="4"></span></div>
-    <div class="shape shape-four scene"><span data-depth=".5"></span></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7">
-                <div class="hero-content">
-                    {{-- <h2 class="text-white" data-aos="fade-up" data-aos-delay="80">Selamat datang di Campus Ease {{ config('app.name') }}</h2> --}}
-                    <h2 class="text-white" data-aos="fade-up" data-aos-delay="80">Selamat datang di {{ web_settings('web', 'website_name') }}</h2>
-                    <p data-aos="fade-up" data-aos-delay="90">{{ web_settings('web', 'home_quotes') }}</p>
-                    <a href="{{route('landing-page.contact.index')}}" class="main-btn btn-gradient-yellow" data-aos="fade-up" data-aos-delay="100">Hubungi Kami</a>
+@section('content')
+    <!--====== Start Hero Section ======-->
+    <section class="hero-area hero-style-three bg_cover"
+        style="background-image: url({{ URL::to('/') }}/templates/landing-page/assets/images/hero/bg.png);">
+        <div class="shape shape-one scene"><span data-depth="3"></span></div>
+        <div class="shape shape-two scene"><span data-depth="5"></span></div>
+        <div class="shape shape-three scene"><span data-depth="4"></span></div>
+        <div class="shape shape-four scene"><span data-depth=".5"></span></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="hero-content">
+                        {{-- <h2 class="text-white" data-aos="fade-up" data-aos-delay="80">Selamat datang di Campus Ease {{ config('app.name') }}</h2> --}}
+                        <h2 class="text-white" data-aos="fade-up" data-aos-delay="80">Selamat datang di
+                            {{ web_settings('web', 'website_name') }}</h2>
+                        <p data-aos="fade-up" data-aos-delay="90">{{ web_settings('web', 'home_quotes') }}</p>
+                        <a href="{{ route('landing-page.contact.index') }}" class="main-btn btn-gradient-yellow"
+                            data-aos="fade-up" data-aos-delay="100">Hubungi Kami</a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-5">
-                <div class="hero-img" data-aos="fade-left" data-aos-delay="50">
-                    <img src="{{ asset('storage/'.web_settings('web', 'home_banner')) }}" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-</section><!--====== End Hero Section ======-->
-
-<section class="features-area pb-30 pt-30">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="block-style-one block-icon-animate animated-hover-icon mb-40" data-aos="fade-up" data-aos-delay="30">
-                    <h2 class="pb-3">Visi Misi</h2>
-                    <div class="row">
-                        <div class="col-lg-8">
-                            {{ web_settings('web', 'principal_welcome') }}
-                        </div>
-                        <div class="col-lg-4">
-                            <img src="{{asset('storage/'.web_settings('web', 'principal_avatar'))}}" alt="" style="width: 100%;height:300px;">
-                            <p class="text-center">{{ web_settings('web', 'principal_name') }}</p>
-                        </div>
+                <div class="col-lg-5">
+                    <div class="hero-img" data-aos="fade-left" data-aos-delay="50">
+                        <img src="{{ asset('storage/' . web_settings('web', 'home_banner')) }}" alt="">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section><!--====== End Hero Section ======-->
 
-<section class="features-area pb-90">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <div class="section-title text-center mb-55" data-aos="fade-up">
-                    <h2>Beasiswa</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            @foreach ($announcements as $index => $row)
-            <div class="col-12 mb-3">
-                <div class="block-style-one">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="{{asset('storage/'.$row->cover)}}" style="width: 100%;height:100px;">
-                            <p class="text-center mt-3"><small>{{date('d F Y',strtotime($row->date))}}</small></p>
-                        </div>
-                        <div class="col-md-9">
-                            <h3>{{$row->title}}</h3>
-                            <p>{{$row->fragment}}</p>
-                            <p class="mt-2"><small>Dipublikasi Pada: {{date('d F Y H:i:s',strtotime($row->published_at))}}</small></p>
-                            <a href="{{route('landing-page.announcements.show',$row->slug)}}" class="btn btn-primary btn-sm mt-3">Baca Selengkapnya</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            @endforeach
-        </div>
-        <div class="row mt-3">
-            <div class="col-12 text-center">
-                <a href="{{route('landing-page.announcements.index')}}" class="main-btn btn-blue-dark">Lihat Semua Beasiswa</a>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="features-area pb-90">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <div class="section-title text-center mb-55" data-aos="fade-up">
-                    <h2>Daftar Universitas</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            @foreach ($daftaruniversitas as $index => $row)
-            <div class="col-lg-4 col-md-6 col-sm-12">
-                <div class="block-style-one block-icon-animate animated-hover-icon mb-40" data-aos="fade-up" data-aos-delay="30">
-                    <a class="image-popup" href="{{asset('storage/'.$row->image)}}">
-                        <img src="{{asset('storage/'.$row->image)}}" alt="{{$row->title}}">
-                    </a>
-                    <p class="text-center mt-1">{{$row->title}}</p>
-                </div>
-            </div>
-            @endforeach
-        </div>
-        <div class="row">
-            <div class="col-12 text-center">
-                <a href="{{route('landing-page.daftaruniversitas.index')}}" class="main-btn btn-blue-dark">Lihat Semua Universitas</a>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="team-area team-style-one pb-90">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <div class="section-title text-center mb-55" data-aos="fade-up">
-                    <h2>Tim Pengembang</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            @foreach($employee as $index => $row)
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
-                <div class="team-item mb-40" data-aos="fade-up" data-aos-delay="30">
-                    <div class="img-holder">
-                        <img src="{{ asset('storage/'.$row->image) }}" alt="Team Image">
-                        <div class="team-hover">
-                            <div class="hover-content text-center">
-                                <h5><a href="#">{{ $row->name }}</a></h5>
-                                <p class="position">{{ $row->position }}</p>
-                                <p class="position"><small>{{ $row->phone }}</small></p>
+    <section class="features-area pb-30 pt-30">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="block-style-one block-icon-animate animated-hover-icon mb-40" data-aos="fade-up"
+                        data-aos-delay="30">
+                        <h2 class="pb-3">Visi Misi</h2>
+                        <div class="row">
+                            <div class="col-lg-8">
+                                {{ web_settings('web', 'principal_welcome') }}
+                            </div>
+                            <div class="col-lg-4 d-flex flex-column align-self-center">
+                                <!-- Gantikan gambar dengan video -->
+                                <div class="embed-responsive embed-responsive-16by9 w-100 mb-3">
+                                    <video class="embed-responsive-item" controls>
+                                        <source src="{{ asset('templates/landing-page/assets/video/vd.mp4') }}" type="video/mp4">
+                                        Browser Anda tidak mendukung tag video.
+                                    </video>
+                                </div>
+                                <p class="text-center mt-2">{{ web_settings('web', 'principal_name') }}</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            @endforeach
         </div>
-        <div class="row">
-            <div class="col-12 text-center">
-                <a href="{{route('landing-page.employees.index')}}" class="main-btn btn-blue-dark">Lihat Semua Anggota</a>
+    </section>
+
+
+    <section class="features-area pb-90">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section-title text-center mb-55" data-aos="fade-up">
+                        <h2>Beasiswa</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                @foreach ($announcements as $index => $row)
+                    <div class="col-12 mb-3">
+                        <div class="block-style-one">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <img src="{{ asset('storage/' . $row->cover) }}" style="width: 100%;height:100px;">
+                                    <p class="text-center mt-3"><small>{{ date('d F Y', strtotime($row->date)) }}</small></p>
+                                </div>
+                                <div class="col-md-9">
+                                    <h3>{{ $row->title }}</h3>
+                                    <p>{{ $row->fragment }}</p>
+                                    <p class="mt-2"><small>Dipublikasi Pada:
+                                            {{ date('d F Y H:i:s', strtotime($row->published_at)) }}</small></p>
+                                    <a href="{{ route('landing-page.announcements.show', $row->slug) }}"
+                                        class="btn btn-primary btn-sm mt-3">Baca Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="row mt-3">
+                <div class="col-12 text-center">
+                    <a href="{{ route('landing-page.announcements.index') }}" class="main-btn btn-blue-dark">Lihat Semua
+                        Beasiswa</a>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+
+    <section class="features-area pb-90">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section-title text-center mb-55" data-aos="fade-up">
+                        <h2>Daftar Universitas</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                @foreach ($daftaruniversitas as $index => $row)
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="block-style-one block-icon-animate animated-hover-icon mb-40" data-aos="fade-up"
+                            data-aos-delay="30">
+                            <a class="image-popup" href="{{ asset('storage/' . $row->image) }}">
+                                <img src="{{ asset('storage/' . $row->image) }}" alt="{{ $row->title }}">
+                            </a>
+                            <p class="text-center mt-1">{{ $row->title }}</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="row">
+                <div class="col-12 text-center">
+                    <a href="{{ route('landing-page.daftaruniversitas.index') }}" class="main-btn btn-blue-dark">Lihat
+                        Semua Universitas</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="team-area team-style-one pb-90">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section-title text-center mb-55" data-aos="fade-up">
+                        <h2>Tim Pengembang</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                @foreach ($employee as $index => $row)
+                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+                        <div class="team-item mb-40" data-aos="fade-up" data-aos-delay="30">
+                            <div class="img-holder">
+                                <img src="{{ asset('storage/' . $row->image) }}" alt="Team Image">
+                                <div class="team-hover">
+                                    <div class="hover-content text-center">
+                                        <h5><a href="#">{{ $row->name }}</a></h5>
+                                        <p class="position">{{ $row->position }}</p>
+                                        <p class="position"><small>{{ $row->phone }}</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="row">
+                <div class="col-12 text-center">
+                    <a href="{{ route('landing-page.employees.index') }}" class="main-btn btn-blue-dark">Lihat Semua
+                        Anggota</a>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
 
-@section("scrolltop")
-    @component("landing-page.components.scrolltop")
+@section('scrolltop')
+    @component('landing-page.components.scrolltop')
     @endcomponent
 @endsection
 
-@section("script")
+@section('script')
 @endsection
-
